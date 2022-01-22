635ab45291783fbea37e612e17add05670f456af
Huang Grace <huang.yahui.grace@gmail.com>
2022-01-22 22:55:28 +0800

    Initial Commit

---
+++ MyTestGitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTestGitApp
+//
+//  Created by grace on 2022/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
